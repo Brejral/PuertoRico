f70d182acb16bb186ff857dd08b757a710f2c9f6
Tim Andrews <timandrews@cox.net>
2014-11-30 17:47:22 -0800

    Better GUI Look, but still Testing

--- desktop/src/com/brejral/puertorico/desktop/DesktopLauncher.java
+++ desktop/src/com/brejral/puertorico/desktop/DesktopLauncher.java
@@ -14,44 +14,62 @@ import javax.swing.JPanel;
 
 import com.brejral.puertorico.game.Game;
 import com.brejral.puertorico.game.GameHelper;
+import com.brejral.puertorico.game.crop.Coffee;
 import com.brejral.puertorico.game.crop.Corn;
+import com.brejral.puertorico.game.crop.Indigo;
+import com.brejral.puertorico.game.crop.Quarry;
+import com.brejral.puertorico.game.crop.Sugar;
+import com.brejral.puertorico.game.crop.Tobacco;
 import com.brejral.puertorico.game.player.Player;
+
 // Tim import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 // Tim import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopLauncher {
 	
-	private static JLabel createLabelPairGUI(JPanel pane, GridBagConstraints c, int gridx, int gridy, String label) {
+    static JLabel supplyCoinsLabel;
+    static JLabel supplyPointsLabel;
+    static JLabel supplySettlersLabel;
+
+    static JLabel supplyGoodsCoffeeLabel ;
+    static JLabel supplyGoodsCornLabel;
+    static JLabel supplyGoodsIndigoLabel;
+    static JLabel supplyGoodsSugarLabel;
+    static JLabel supplyGoodsTobaccoLabel;
+    static JLabel supplyGoodsQuarryLabel;
+
+	private static JLabel createLabelPairGUI(JPanel pane, GridBagConstraints c, int gridx, int gridy, int gridw, String label) {
 		JPanel panel = new JPanel(new FlowLayout());
 		JLabel jlabel = new JLabel(label);
 		JLabel field = new JLabel("0");
 		panel.add(jlabel);
 		panel.add(field);
 		c.fill = GridBagConstraints.HORIZONTAL;
+		// c.weightx = 1.0;   //request any extra vertical space
+		// c.gridx = (gridx*2);
 		c.gridx = gridx;
 		c.gridy = gridy;
+		c.gridwidth = gridw;
+		// pane.add(jlabel, c);
+		// c.fill = GridBagConstraints.HORIZONTAL;
+		// c.weightx = 1.0;   //request any extra vertical space
+		// c.gridx = (gridx*2)+1;
+		// c.gridy = gridy;
+		// pane.add(field, c);
 		pane.add(panel, c);
 		return field;
 	}
 
-	private static void createSummaryGUI(JFrame frame) {
-		JButton button;
-		
-		JPanel pane = new JPanel(new GridBagLayout());
-		pane.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		// Tim if (shouldFill) {
-		                //natural height, maximum width
-		                // Tim c.fill = GridBagConstraints.HORIZONTAL;
-		// Tim }
+    private static void updateAll()
+    {
+        updateSupplies();
+        updateGoods();
+	}
 
-        JLabel supplyCoinsLabel = createLabelPairGUI(pane, c, 0, 0, "Coins");
-        JLabel supplyPointsLabel = createLabelPairGUI(pane, c, 1, 0, "Points");
-        JLabel supplySettlersLabel = createLabelPairGUI(pane, c, 2, 0, "Settlers");
-
-        // int supplyCoins = game.getBank().getCoinSupply();
-        // supplyCoinsLabel.setText(Integer.toString(supplyCoins));
-
+    private static void updateSupplies()
+    {
+    	// Supply Fields
+    	// Display the Number of Main Supplies Remaining
         int supplyCoins = GameHelper.getBank().getCoinSupply();
         supplyCoinsLabel.setText(Integer.toString(supplyCoins));
 
@@ -61,39 +79,79 @@ public class DesktopLauncher {
         int supplySettlers = GameHelper.getBank().getSettlerSupply();
         supplySettlersLabel.setText(Integer.toString(supplySettlers));
 
-		int supplyCorn = GameHelper.getBank().getGoodSupply().get(Corn.NAME);
+		List<Quarry> supplyGoodsQuarryList = GameHelper.getBank().getQuarrySupply();
+        supplyGoodsQuarryLabel.setText(Integer.toString(supplyGoodsQuarryList.size()));
+	}
 
-		// Tim button = new JButton("Button 1");
-		// Tim if (shouldWeightX) {
-		                   // Tim c.weightx = 0.5;
-		// Tim }
-		// c.fill = GridBagConstraints.HORIZONTAL;
-		// Tim c.gridx = 0;
-		// Tim c.gridy = 0;
-		// Tim pane.add(button, c);
+    private static void updateGoods()
+    {
+        // Display the number Goods Remaining
+        int supplyGoodsCoffee = GameHelper.getBank().getGoodSupply().get(Coffee.NAME);
+        supplyGoodsCoffeeLabel.setText(Integer.toString(supplyGoodsCoffee));
 
-		// Tim button = new JButton("Button 2");
-		// Tim c.fill = GridBagConstraints.HORIZONTAL;
-		// Tim c.weightx = 0.5;
-		// Tim c.gridx = 1;
-		// Tim c.gridy = 0;
-		// Tim pane.add(button, c);
+        int supplyGoodsCorn = GameHelper.getBank().getGoodSupply().get(Corn.NAME);
+        supplyGoodsCornLabel.setText(Integer.toString(supplyGoodsCorn));
 
-		// Tim button = new JButton("Button 3");
-		// Tim c.fill = GridBagConstraints.HORIZONTAL;
-		// Tim c.weightx = 0.5;
-		// Tim c.gridx = 2;
-		// Tim c.gridy = 0;
-		// Tim pane.add(button, c);
+        int supplyGoodsIndigo = GameHelper.getBank().getGoodSupply().get(Indigo.NAME);
+        supplyGoodsIndigoLabel.setText(Integer.toString(supplyGoodsIndigo));
 
+        int supplyGoodsSugar = GameHelper.getBank().getGoodSupply().get(Sugar.NAME);
+        supplyGoodsSugarLabel.setText(Integer.toString(supplyGoodsSugar));
+
+        int supplyGoodsTobacco = GameHelper.getBank().getGoodSupply().get(Tobacco.NAME);
+        supplyGoodsTobaccoLabel.setText(Integer.toString(supplyGoodsTobacco));
+
+    }
+
+	private static void createSummaryGUI(JFrame frame) {
+		JButton button;
+		int col = 0;
+		int row = 0;
+		
+		JPanel pane = new JPanel(new GridBagLayout());
+		pane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		JLabel jlabel = new JLabel("Supplies:");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = col++;
+		c.gridy = row;
+		c.gridwidth = 1;
+		pane.add(jlabel, c);
+
+        supplyCoinsLabel = createLabelPairGUI(pane, c, col++, row, 1, "Coins");
+        supplyPointsLabel = createLabelPairGUI(pane, c, col++, row, 1, "Points");
+        supplySettlersLabel = createLabelPairGUI(pane, c, col++, row, 1, "Settlers");
+        supplyGoodsQuarryLabel  = createLabelPairGUI(pane, c, col++, row, 1, Quarry.NAME);
+        row++;
+
+		col = 0;
+		jlabel = new JLabel("Goods:");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = col++;
+		c.gridy = row;
+		c.gridwidth = 1;
+		pane.add(jlabel, c);
+
+        supplyGoodsCoffeeLabel  = createLabelPairGUI(pane, c, col++, row, 1, Coffee.NAME);
+        supplyGoodsCornLabel    = createLabelPairGUI(pane, c, col++, row, 1, Corn.NAME);
+        supplyGoodsIndigoLabel  = createLabelPairGUI(pane, c, col++, row, 1, Indigo.NAME);
+        supplyGoodsSugarLabel   = createLabelPairGUI(pane, c, col++, row, 1, Sugar.NAME);
+        supplyGoodsTobaccoLabel = createLabelPairGUI(pane, c, col++, row, 1, Tobacco.NAME);
+        row++;
+
+        updateAll();
+
+        /*
 		button = new JButton("Long-Named Button 4");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.ipady = 40;      //make this component tall
 		c.weightx = 0.0;
 		c.gridwidth = 3;
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = row;
 		pane.add(button, c);
+        row++;
 
 		button = new JButton("5");
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -103,8 +161,10 @@ public class DesktopLauncher {
 		c.insets = new Insets(10,0,0,0);  //top padding
 		c.gridx = 1;       //aligned with button 2
 		c.gridwidth = 2;   //2 columns wide
-		c.gridy = 2;       //third row
+		c.gridy = row;       //third row
 		pane.add(button, c);
+        row++;
+        */
 
         frame.getContentPane().add(pane);
 	}
@@ -119,9 +179,6 @@ public class DesktopLauncher {
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        //Add the ubiquitous "Hello World" label.
-        // Tim JLabel label = new JLabel("Hello World");
-        // Tim frame.getContentPane().add(label);
         createSummaryGUI(frame);
  
         //Display the window.
@@ -137,7 +194,6 @@ public class DesktopLauncher {
     	{
     		players.add(new Player());
     	}
-    	// game = new Game(players);
     	new Game(players);
     	
         createAndShowGUI();
