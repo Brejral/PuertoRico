5ce2f238f75ee2cdd049bf98f4a7e5f7cd96aa8a
Brejral <tcandrews32@gmail.com>
2014-12-04 15:53:17 -0800

    Trader Update

--- core/src/com/brejral/puertorico/game/role/Trader.java
+++ core/src/com/brejral/puertorico/game/role/Trader.java
@@ -4,30 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.brejral.puertorico.game.GameHelper;
+import com.brejral.puertorico.game.building.Office;
 import com.brejral.puertorico.game.crop.Crop;
 import com.brejral.puertorico.game.player.Player;
 
 public class Trader extends Role {
 	public static final String NAME = "Trader";
-	private List<Crop> tradedCrops = new ArrayList<Crop>(4);
+	private List<String> tradedCrops = new ArrayList<String>(4);
 
 	public Trader() {
 		super(NAME);
 	}
 	
 	public void onRoleStart() {
-		Player player = GameHelper.getCurrentPlayerForTurn();
-		super.onRoleStart(player);
+		super.onRoleStart();
 	}
 	
-	public void onRoleAction(Crop crop) {
+	public void onAction(String cropName) {
 		Player player = GameHelper.getCurrentPlayerForAction();
+		tradedCrops.add(cropName);
+		player.subtractGood(cropName, 1);
+		GameHelper.addGoodSupply(cropName, 1);
+		super.onAction();
 	}
 	
-	public List<Crop> getTradableCropsForCurrentPlayer() {
-		List<Crop> cropsToTrade = new ArrayList<Crop>();
+	public List<String> getTradableCropsForCurrentPlayer() {
+		List<String> cropsToTrade = new ArrayList<String>();
 		Player player = GameHelper.getCurrentPlayerForAction();
-		
+		for (String cropName : Crop.CROP_LIST) {
+			if (player.getNumberOfGoods(cropName) > 0 && (!hasCropBeenTraded(cropName)) || player.hasActiveBuilding(Office.NAME)) {
+				cropsToTrade.add(cropName);
+			}
+		}
 		return cropsToTrade;
+	}
+	
+	private boolean hasCropBeenTraded(String cropName) {
+		return tradedCrops.contains(cropName);
 	}
 }
